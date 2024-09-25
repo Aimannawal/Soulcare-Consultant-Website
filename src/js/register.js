ea8cd6430ba7c9c916aee749f5407c20d27f9f26
Aimannawal <aone111181@gmail.com>
2024-09-25 21:37:05 +0700

    feat: add register

---
+++ src/js/register.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function () {
+    document.querySelector('.form-section').classList.add('visible');
+    document.querySelector('.illustration-section').classList.add('visible');
+});
